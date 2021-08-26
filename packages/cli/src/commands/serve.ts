6733a23bcd5e42686c1f36d229e74efb3224cb47
lukezzz <sd65745@126.com>
2021-08-26 09:01:49 +0800

    add cli and express

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -1,8 +1,26 @@
+import path from 'path'
 import { Command } from 'commander'
+import { serve } from 'local-api'
 
 export const serveCommand = new Command()
-    .command('serve')
+    .command('serve [filename]')
     .description('Open a file for editing')
-    .action(() => {
-        console.log('Getting ready to serve a file')
+    .option('-p, --port <number>', 'port to run server on', '4005')
+    .action(async (filename = 'notebook.js', options: { port: string }) => {
+        // console.log(filename, options)
+        try {
+            const dir = path.join(process.cwd(), path.dirname(filename))
+            await serve(parseInt(options.port), path.basename(filename), dir)
+            console.log(
+                `Opened ${filename}. Navigate to http://localhost:${options.port} to edit the file.`
+            )
+
+        } catch (err) {
+            if (err.code === 'EADDRINUSE') {
+                console.log('Port is in use. Try running on a different port.')
+            } else {
+                console.log('Heres the problem', err.message)
+            }
+            process.exit(1)
+        }
     })
--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,3 +1,20 @@
-export default () => {
-    console.log('Server is listening')
+import express from 'express'
+import { createProxyMiddleware } from 'http-proxy-middleware'
+export const serve = (port: number, filename: string, dir: string) => {
+
+    // console.log('serving traffic on port', port)
+    // console.log('saving/fetching cells from', filename)
+    // console.log('that file is in dir', dir)
+    const app = express()
+
+    app.use(createProxyMiddleware({
+        target: 'http://localhost:3000',
+        ws: true,
+        logLevel: 'silent'
+    }))
+
+    return new Promise<void>((resolve, reject) => {
+        app.listen(port, resolve).on('error', reject)
+    })
+
 }
